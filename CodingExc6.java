2aab90114e937ad2a63c88dbf0a8436ed9efdbf0
Bhuto1998 <arnab1998tyt17@gmail.com>
2020-05-11 13:23:29 +0530

    reg-up-11

--- CodingExc6.java
+++
@@ -1,9 +0,0 @@
-public class CodingExc6 {
-    public static void main(String[] args) {
-        System.out.println(hasEqualSum(3,2,5));
-    }
-    public static boolean hasEqualSum(int x , int y, int z){
-        return x+y==z;
-
-    }
-}
